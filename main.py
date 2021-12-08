c03a8ec7da1195c99328747797c1588b84afcff0
lymacasm <lyden.m.smith@gmail.com>
2021-12-07 20:36:50 -0800

    Adding debug code for server.

--- main.py
+++ main.py
@@ -1,11 +1,20 @@
 import os
+import sys
 from dotenv import load_dotenv
 from discord.ext import commands
 
+print('Loading dotenv')
 load_dotenv()
-WRITING_BOT_TOKEN = os.getenv('DISCORD_WRITING_BOT_TOKEN')
+try:
+    WRITING_BOT_TOKEN = os.getenv('DISCORD_WRITING_BOT_TOKEN')
+except:
+    sys.exit('Failed to grab environment variable.')
 
+print('Getting bot setup.')
 writing_bot = commands.Bot(command_prefix='!')
+print('Loading extension 1')
 writing_bot.load_extension('cogs.writing_greetings')
+print('Loading extension 2')
 writing_bot.load_extension('cogs.thesaurus_cmds')
+print('Starting bot.')
 writing_bot.run(WRITING_BOT_TOKEN)
